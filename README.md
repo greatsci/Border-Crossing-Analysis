62675496742c04160760b114b88d518146abc6d1
greatsci <ningz615@gmail.com>
2020-02-20 17:54:59 -0600

    Update

--- README.md
+++ README.md
@@ -1,5 +1,38 @@
-# NingZhang2020
- Insight data challenge
+# Border Crossing Analysis
+## NingZhang Insight data challenge 2020
  
- Here we have the resource of border crossing
+## Description
+The Bureau of Transportation Statistics regularly makes available data on the number of vehicles, equipment, passengers and pedestrians crossing into the United States by land.
+
+The python script in ./src calculates the total number of times vehicles, equipment, passengers and pedestrians cross the U.S.-Canadian and U.S.-Mexican borders each month. It also gives the running monthly average of total number of crossings for that type of crossing and border.
+
+## Link submitted
+https://github.com/greatsci/NingZhang2020
+
+## Repo directory structure
+
+The directory structure for your repo should look like this:
+
+    ├── README.md
+    ├── run.sh
+    ├── src
+    │   └── border_analytics.py
+    ├── input
+    │   └── Border_Crossing_Entry_Data.csv
+    ├── output
+    |   └── report.csv
+    ├── insight_testsuite
+        └── run_tests.sh
+        └── results.txt
+        └── tests
+            └── test_1
+            |   ├── input
+            |   │   └── Border_Crossing_Entry_Data.csv
+            |   |__ output
+            |   │   └── report.csv
+            ├── test_larger_input
+                ├── input
+                │   └── Border_Crossing_Entry_Data.csv
+                |── output
+                    └── report.csv
 
--- src/border_analytics.py
+++ src/border_analytics.py
@@ -1,29 +1,34 @@
 import csv
 import sys
+
+# Open input csv file
 f = open(sys.argv[1])
 csv_f = csv.reader(f)
-myDict = {}
-# Use flag to avoid title line
+inputDict = {}
+# Use flag to avoid table header
 flag = 1
+
 for row in csv_f:
-    # Extract 'Border', 'Date',' Measure' information from each entry
+    # Extract 'Border', 'Date',' Measure' from each entry
     if flag:
         flag = 0
         continue
     temp = tuple([row[3], row[4], row[5]])
     num = int(row[6])
-    if temp in myDict:
-        myDict[temp] += num
+    # Sum up the total value for each category
+    if temp in inputDict:
+        inputDict[temp] += num
     else:
-        myDict[temp] = num
+        inputDict[temp] = num
 
 lst = []
-for key, value in myDict.items():
+for key, value in inputDict.items():
     lst.append([key[0], key[1], key[2], value, 0])
 
 # x[0]: Border; x[1]: Date; x[2]: Measure; x[3]: Value
 sortedlst = sorted(lst, key = lambda x : (x[1], x[3], x[2], x[0]))
 
+# Calculate moving average for all previous months
 avgDict = {}
 for item in sortedlst:
     temp2 = tuple([item[0], item[2]])
@@ -36,12 +41,11 @@ for item in sortedlst:
         avgDict[temp2] = [item[3] + 1e-10, 1]
 
 sortedlst = sorted(lst, key = lambda x : (x[1], x[3], x[2], x[0]), reverse = True)
-print(sortedlst)
 
+# Add header line
 results = [['Border', 'Date', 'Measure', 'Value', 'Average']]
 for res in sortedlst:
     results.append(res)
-# print(results)
 
 with open(sys.argv[2], "w", newline = "") as f:
     cw = csv.writer(f)
